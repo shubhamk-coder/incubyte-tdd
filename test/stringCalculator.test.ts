d0e994292943f7d92ce00d56e4f47fb99598ebed
Shubham Kamble <shubhamkamble8983@gmail.com>
2024-10-13 19:07:32 +0530

    Test 5: Support new lines between numbers

--- test/stringCalculator.test.ts
+++ test/stringCalculator.test.ts
@@ -14,8 +14,14 @@ describe("String Calculator", () => {
     expect(add("1,2")).toBe(3);
     expect(add("3,5")).toBe(8);
   });
+
   it("should return the sum of multiple numbers", () => {
     expect(add("1,2,3")).toBe(6);
     expect(add("4,5,6,7")).toBe(22);
+  });
+
+  it("should handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("1\n2\n3")).toBe(6);
   });
 });
